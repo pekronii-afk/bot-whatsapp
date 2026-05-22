56ca586c9dcb06eb2727b33e3d5f589e9c7d34ae
pekronii-afk <pekronii@gmail.com>
2026-05-22 11:46:37 -0700

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -54,4 +55,5 @@ def recibir_mensaje():
     return jsonify({"status": "ok"}), 200
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
